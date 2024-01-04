865fd87d6b6345f845c0820774d17747dc08086a
Sasha1-0-0-9 <34281985+Sasha1-0-0-9@users.noreply.github.com>
2024-01-04 10:46:51 +0200

    Add files via upload

---
+++ parallel_task/src/main/java/org/ppjava1/Main.java
@@ -0,0 +1,9 @@
+package org.ppjava1;
+
+public class Main {
+    public static void main(String[] args) {
+        parSum parSum = new parSum(100000);
+        parSum.sum();
+        parSum.shutdown();
+    }
+}
